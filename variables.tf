61e68f42fb5cb12f74867c2dfeac6ab83b92d12a
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-20 18:51:23 +0530

    test

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "env" {}
 variable "tags" {}
 variable "subnet_ids" {}
+variable "port" {}
+variable "sg_ingress_cidr" {}
+variable "vpc_id" {}
+variable "family" {}
+variable "engine_version" {}
+variable "num_cache_nodes" {}
+variable "node_type" {}
+variable "engine" {}
