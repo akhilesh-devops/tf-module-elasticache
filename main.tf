944c890cf274d6a86ca896c6820fb7588817838f
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-20 19:13:47 +0530

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_elasticache_subnet_group" "main" {
   name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.subnet_ids
 
-  tags = merge(local,tags, { Name = "${local.name_prefix}-subnet-group" })
+  tags = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
 }
 
 resource "aws_security_group" "main" {
